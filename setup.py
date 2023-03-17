173dd339c72ead21efe0e60fe9792719ce44e07f
sonyakun <90973770+sonyakun@users.noreply.github.com>
2023-03-17 14:34:47 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ long_description_content_type = "https://github.com/sonyakun/sakurapkg"
 
 setup(
     name=NAME,
-      version="0.1.0",
+      version="0.1.1",
       author=AUTHOR,
       author_email=AUTHOR_EMAIL,
       maintainer=AUTHOR,
